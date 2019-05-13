f3038d867ad701c3379bb64a74734d57a532074e
zaak94 <rogelioisaac3@hotmail.com>
2019-05-13 01:30:30 -0500

    actualiza informacion

--- app/src/main/java/com/example/recycleview2/MainActivity.kt
+++ app/src/main/java/com/example/recycleview2/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.recycleview2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import java.util.ArrayList
@@ -27,12 +29,7 @@ class MainActivity : AppCompatActivity() {
         platillos.add(Platillo("Platillo 2", 200.0, 4.0F, R.drawable.im2  ))
         platillos.add(Platillo("Platillo 3", 150.0, 5.0F,R.drawable.im3  ))
         platillos.add(Platillo("Platillo 4", 50.0, 2.5F, R.drawable.im4  ))
-        platillos.add(Platillo("Platillo 5", 750.0, 1.5F,R.drawable.im5  ))
-        platillos.add(Platillo("Platillo 6", 250.0, 3.0F, R.drawable.im6  ))
-        platillos.add(Platillo("Platillo 7", 200.0, 4.0F, R.drawable.im7  ))
-        platillos.add(Platillo("Platillo 8", 150.0, 5.0F,R.drawable.im8  ))
-        platillos.add(Platillo("Platillo 9", 50.0, 2.5F, R.drawable.im9  ))
-        platillos.add(Platillo("Platillo 10", 750.0, 1.5F,R.drawable.im10  ))
+
 
         lista = findViewById(R.id.lista)
         lista?.setHasFixedSize(true)
@@ -47,5 +44,19 @@ class MainActivity : AppCompatActivity() {
 
         })
        lista?.adapter = adaptador
+
+        val swipeToRefresh = findViewById<SwipeRefreshLayout>(R.id.swipeToRefresh)
+        swipeToRefresh.setOnRefreshListener {
+           for(i in 1..300000000){
+           }
+            swipeToRefresh.isRefreshing=false
+            platillos.add(Platillo("Platillo 5", 750.0, 1.5F,R.drawable.im5  ))
+            platillos.add(Platillo("Platillo 6", 250.0, 3.0F, R.drawable.im6  ))
+            platillos.add(Platillo("Platillo 7", 200.0, 4.0F, R.drawable.im7  ))
+            platillos.add(Platillo("Platillo 8", 150.0, 5.0F,R.drawable.im8  ))
+            platillos.add(Platillo("Platillo 9", 50.0, 2.5F, R.drawable.im9  ))
+            platillos.add(Platillo("Platillo 10", 750.0, 1.5F,R.drawable.im10  ))
+            adaptador?.notifyDataSetChanged()
+        }
     }
 }
